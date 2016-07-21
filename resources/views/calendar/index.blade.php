6ef33bfa42c325c844f8909498918662e25625f2
jgsantos <joao.guilherme@crafti.com.br>
2016-07-21 00:45:11 -0300

    started adding filters on the calendar

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -6,6 +6,33 @@
 
   <div class="container" id="app">
     <h1>Agenda</h1>
+
+    <form class="form-inline" action="" method="post">
+      <div class="form-group">
+        <label for="professional_id">Professional: </label>
+        <select class="form-control" name="professional_id">
+          <option value="all">All</option>
+          <option value="option">Bruna</option>
+        </select>
+      </div>
+
+      <div class="form-group">
+        <label for="professional_id">Class: </label>
+        <select class="form-control" name="professional_id">
+          <option value="all">All</option>
+          <option value="option">Bruna</option>
+        </select>
+      </div>
+
+      <div class="form-group">
+        <label for="professional_id">Room: </label>
+        <select class="form-control" name="professional_id">
+          <option value="all">All</option>
+          <option value="option">Bruna</option>
+        </select>
+      </div>
+    </form>
+
     {!! $calendar->calendar() !!}
     <!-- use the modal component, pass in the prop -->
     <modal :show.sync="showModal" :header.sync="headerTitle">
