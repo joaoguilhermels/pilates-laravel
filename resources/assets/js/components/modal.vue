e6a2d0e53096fa2e7042e7577e3f943fedf44227
jgsantos <joao.guilherme@crafti.com.br>
2017-01-08 17:06:18 -0200

    adicionei adminlte e bootswatch

---
+++ resources/assets/js/components/modal.vue
@@ -0,0 +1,27 @@
+<template id="schedule-update-template">
+    <div class="modal fade" id="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button " class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+
+                    <slot name="modal-title"></slot>
+                </div>
+
+                <slot name="modal-body"></slot>
+
+                <!-- Modal Actions -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        ready () {
+            //alert('asdf');
+        }
+    }
+</script>
