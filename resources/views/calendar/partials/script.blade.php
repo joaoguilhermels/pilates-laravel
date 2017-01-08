b5bf74f0f22946379c95161af16d34e7ee901808
jgsantos <joao.guilherme@crafti.com.br>
2017-01-08 17:05:05 -0200

    updates

--- resources/views/calendar/partials/script.blade.php
+++ resources/views/calendar/partials/script.blade.php
@@ -1,7 +1,7 @@
 @section('script_footer')
   <script src="//cdnjs.cloudflare.com/ajax/libs/qtip2/3.0.3/jquery.qtip.min.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.0/moment.min.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.0.0/fullcalendar.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.0.0/locale/pt-br.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/locale/pt-br.js"></script>
   {!! $calendar->script() !!}
 @stop
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,22 +5,26 @@
     
     <div class="row">
         <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
-            <div class="info-box-content">
-              <span class="info-box-text">Clients</span>
-              <span class="info-box-number">{{ $clients }}</span>
-            </div><!-- /.info-box-content -->
-          </div><!-- /.info-box -->
+          <a href="clients">
+            <div class="info-box">
+              <span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text">Clients</span>
+                <span class="info-box-number">{{ $clients }}</span>
+              </div><!-- /.info-box-content -->
+            </div><!-- /.info-box -->
+          </a>
         </div><!-- /.col -->
         <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-green"><i class="fa fa-flag-o"></i></span>
-            <div class="info-box-content">
-              <span class="info-box-text">Professionals</span>
-              <span class="info-box-number">{{ $professionals }}</span>
-            </div><!-- /.info-box-content -->
-          </div><!-- /.info-box -->
+          <a href="professionals">
+            <div class="info-box">
+              <span class="info-box-icon bg-green"><i class="fa fa-flag-o"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text">Professionals</span>
+                <span class="info-box-number">{{ $professionals }}</span>
+              </div><!-- /.info-box-content -->
+            </div><!-- /.info-box -->
+          </a>
         </div><!-- /.col -->
         <div class="col-md-3 col-sm-6 col-xs-12">
           <div class="info-box">
